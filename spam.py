8158ae7fe1bcb8b973fdefcdc19e93c777397e5f
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-03 14:28:29 -0600

    Adding Dockerfile.

--- spam.py
+++ spam.py
@@ -16,16 +16,13 @@ def auth_user():
 
 @app.route('/usage', methods=['GET'])
 def usage():
-    ## not sure what the QS will be valid here
     ## we need this to log how many requests someone has made <= 30 days
     ## there probably is a library to do this 30 day calculation
     ## 30 days X 24 hours x 60 minutes x 60 seconds = total seconds
-    ##
-    data = request
-    data
-    return "You have used `{}`", data
-
-
+    user = request.args.get('user')
+    ## Go to db and figure out how many requests user has made
+    ## in last 30 days. If over plan limit, return 401.
+    return "You are logged in as `{0}`".format(user), 200
 
 @app.route('/spam', methods=['POST'])
 def spam_detection():
@@ -33,9 +30,6 @@ def spam_detection():
     ## We should run the classification SVM here
     ## We will use scikit-learn to do the SVM
     return jsonify({'response': 200, 'results': data})
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
