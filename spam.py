47dd593370c1a44c467c661753a83de92f7e891a
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-03 14:28:17 -0500

    Storing work from airplane. Working on auth and usage API.

--- spam.py
+++ spam.py
@@ -8,11 +8,34 @@ app = Flask(__name__)
 def example():
     return "The API Call Was Completed, Thank You For Using Yancy", 200
 
+@app.route('/auth', methods=['POST'])
+def auth_user():
+    data = request.body
+    print data
+    return "Your Authorization Was Completed, You're now logged in", 200
 
-@app.route('/api', methods=['POST'])
+@app.route('/usage', methods=['GET'])
+def usage():
+    ## not sure what the QS will be valid here
+    ## we need this to log how many requests someone has made <= 30 days
+    ## there probably is a library to do this 30 day calculation
+    ## 30 days X 24 hours x 60 minutes x 60 seconds = total seconds
+    ##
+    data = request
+    data
+    return "You have used `{}`", data
+
+
+
+@app.route('/spam', methods=['POST'])
 def spam_detection():
     data = request.json
+    ## We should run the classification SVM here
+    ## We will use scikit-learn to do the SVM
     return jsonify({'response': 200, 'results': data})
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
