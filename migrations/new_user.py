0e7331b2583666227a457cb073f17dfa475ba0fc
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-03 22:03:32 -0600

    adding new user scripts.

--- migrations/new_user.py
+++ migrations/new_user.py
@@ -2,16 +2,21 @@ import psycopg2
 import psycopg2.extras as e
 from flask import Response
 
-def connection():
+email = 'edwardsdalton97@gmail.com'
+website = 'www.console.chat'
+add_new_user = 'INSERT INTO USERS(email, website) VALUES("{0}", "{1}")'.format(email, website)
+
+print add_new_user
+
+def add_new():
     connection = "host='yancy.c89ytzifs5b6.us-east-1.rds.amazonaws.com' dbname='kam' user='kam' password='Kales333' port='5432'"
     conn = psycopg2.connect(connection)
     cursor = conn.cursor(cursor_factory=e.RealDictCursor)
-    #cursor.execute(sql)
-    #rows = cursor.fetchall()
-    #conn.commit()
+    cursor.execute("INSERT INTO USERS(email, website) VALUES('insert_your_email', 'insert_your_website')")
+    conn.commit()
     cursor.close()
     conn.close()
-    print "Connection Run"
     return Response, 200
 
-connection()
+
+add_new()
--- server.py
+++ server.py
@@ -7,7 +7,6 @@ from db import *
 app = Flask(__name__)
 app.register_blueprint(routes)
 
-db_connection()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
