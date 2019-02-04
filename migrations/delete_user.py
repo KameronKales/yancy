4d7072383b5b38b2f02df815a4a02de92a3ccfc8
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-04 11:14:57 -0600

    Adding delete and new_user scripts.

--- migrations/delete_user.py
+++ migrations/delete_user.py
@@ -2,17 +2,16 @@ import psycopg2
 import psycopg2.extras as e
 from flask import Response
 
-email = 'kameron.kales@gmail.com'
-website = 'www.glance.ai'
+email = 'edwardsdalton97@gmail.com'
 
-def add_new(email, website):
+def delete_user(email):
     connection = "host='yancy.c89ytzifs5b6.us-east-1.rds.amazonaws.com' dbname='kam' user='kam' password='Kales333' port='5432'"
     conn = psycopg2.connect(connection)
     cursor = conn.cursor(cursor_factory=e.RealDictCursor)
-    cursor.execute("INSERT INTO USERS(email, website) VALUES('{0}', '{1}')".format(email, website))
+    cursor.execute("delete from users where email ='{0}'".format(email))
     conn.commit()
     cursor.close()
     conn.close()
     return Response, 200
 
-add_new(email, website)
+delete_user(email)
