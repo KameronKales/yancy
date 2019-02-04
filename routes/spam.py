7fba03c346339bbb10fd523880ec08e0b1f849d6
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-04 11:23:45 -0600

    Adding error codes to api.

--- routes/spam.py
+++ routes/spam.py
@@ -9,24 +9,31 @@ def spam():
     connection = "host='yancy.c89ytzifs5b6.us-east-1.rds.amazonaws.com' dbname='kam' user='kam' password='Kales333' port='5432'"
     conn = psycopg2.connect(connection)
     cursor = conn.cursor(cursor_factory=e.RealDictCursor)
-    api_key = request.json['api_key']
-    spam = request.json['spam']
-    classification = True
-    print spam
-    ## insert spam into spam table with uuid = api_key
-    ## spam table has 3 columns. uuid, content, classification
-    ## uuid = api_key (varchar 12 character limit)
-    ## content = the actual spam we want to classify (varchar 500,000 character limit)
-    ## classification = the classifier we ran (boolean)
-    sql = "INSERT INTO spam(uuid, content, classification) VALUES('{0}', '{1}', '{2}')".format(api_key, spam, classification)
-    cursor.execute(sql)
-    if not cursor.rowcount:
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return jsonify({'response': 401, 'results': 'Unauthorized'})
+    print request.json
+    if 'api_key' not in request.json:
+        return jsonify({'response': 401, 'results': 'Please add your api_key. If you are unsure how please contact support'})
     else:
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return jsonify({'response': 200})
+        if 'spam' not in request.json:
+            return jsonify({'response': 401, 'results': 'Please add your spam to be classified. If you are unsure how please contact support'})
+        else:    
+            api_key = request.json['api_key']
+            spam = request.json['spam']
+            classification = True
+            print spam
+            ## insert spam into spam table with uuid = api_key
+            ## spam table has 3 columns. uuid, content, classification
+            ## uuid = api_key (varchar 12 character limit)
+            ## content = the actual spam we want to classify (varchar 500,000 character limit)
+            ## classification = the classifier we ran (boolean)
+            sql = "INSERT INTO spam(uuid, content, classification) VALUES('{0}', '{1}', '{2}')".format(api_key, spam, classification)
+            cursor.execute(sql)
+            if not cursor.rowcount:
+                conn.commit()
+                cursor.close()
+                conn.close()
+                return jsonify({'response': 401, 'results': 'Unauthorized'})
+            else:
+                conn.commit()
+                cursor.close()
+                conn.close()
+                return jsonify({'response': 200})
