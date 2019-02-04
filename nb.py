a6c5095e2a4b6a38b0667992d67d5e354651f675
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-04 14:14:59 -0600

    SVM works, albeit poorly.

--- nb.py
+++ nb.py
@@ -1,15 +1,37 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-from math import log, sqrt
 import pandas as pd
 import numpy as np
-import re
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn import preprocessing
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
-def classifier(spam):
-    print "This function ran from nb.py"
-    result = 'False'
-    return result
+spamdata = pd.read_csv("spam.csv")
+
+X = spamdata['v2']
+y = spamdata['v1']
+
+le = preprocessing.LabelEncoder()
+if X.dtype == object:
+    X = le.fit_transform(X)
+    #print type(X)
+    X = X.reshape(-1, 1)
+else:
+    pass
+
+print X
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train, y_train)
+y_pred = svclassifier.predict(X_test)
+print y_pred
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+print(accuracy_score)
