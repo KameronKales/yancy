62bb3f2e84472d48bb234d5d5b3d968a569aca09
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-03 22:35:27 -0600

    Auth API

--- migrations/new_user.py
+++ migrations/new_user.py
@@ -2,12 +2,6 @@ import psycopg2
 import psycopg2.extras as e
 from flask import Response
 
-email = 'edwardsdalton97@gmail.com'
-website = 'www.console.chat'
-add_new_user = 'INSERT INTO USERS(email, website) VALUES("{0}", "{1}")'.format(email, website)
-
-print add_new_user
-
 def add_new():
     connection = "host='yancy.c89ytzifs5b6.us-east-1.rds.amazonaws.com' dbname='kam' user='kam' password='Kales333' port='5432'"
     conn = psycopg2.connect(connection)
--- routes/auth.py
+++ routes/auth.py
@@ -2,11 +2,26 @@ from flask import Flask, request, jsonify, Response
 import json
 from . import routes
 from pprint import pprint
-VERIFY_TOKEN = 'kameronkales'
+import psycopg2
+import psycopg2.extras as e
+from flask import Response
+
 
 @routes.route("/auth", methods=['GET'])
 def handle_verification():
-    if request.args['hub.verify_token'] == VERIFY_TOKEN:
-        return request.args['hub.challenge']
+    connection = "host='yancy.c89ytzifs5b6.us-east-1.rds.amazonaws.com' dbname='kam' user='kam' password='Kales333' port='5432'"
+    conn = psycopg2.connect(connection)
+    cursor = conn.cursor(cursor_factory=e.RealDictCursor)
+    api_key = request.args.get('api')
+    sql = "SELECT email, website FROM users where uuid_ = '{0}'".format(api_key)
+    cursor.execute(sql)
+    if not cursor.rowcount:
+        print "No results found"
+        return jsonify({'response': 401, 'results': 'Unauthorized'})
     else:
-        return "Invalid verification token"
+        user_info = cursor.fetchall()
+        print user_info
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'response': 200, 'results': user_info})
