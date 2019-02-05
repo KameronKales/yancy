9028377e8cda4af4dd05978130c74ad1d64ddd00
Kameron Kales <kameronkales@Kamerons-MacBook-Pro.local>
2019-02-05 10:42:41 -0600

    Switched to MNB.

--- nb.py
+++ nb.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,31 +14,27 @@ import string
 import nltk
 from sklearn import svm
 from nltk.corpus import stopwords
-from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
-
+from sklearn.metrics import average_precision_score
 spamdata = pd.read_csv("spam.csv")
 
 y = spamdata['v1']
 X = spamdata['v2']
 
-le = preprocessing.LabelEncoder()
-if X.dtype == object:
-    X = le.fit_transform(X)
-    X = X.reshape(-1, 1)
-else:
-    pass
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= .20, random_state = 0)
-clf = MultinomialNB().fit(X_train, y_train)
-s = pickle.dumps(clf)
-clf2 = pickle.loads(s)
-print clf2.score(X, y)
-spam = ["Kameron Kales was here"]
-spam = le.fit_transform(spam)
-print spam
-print spam.reshape(-1, 1)
-print clf2.predict([spam])
+cv = TfidfVectorizer(min_df=1,stop_words='english')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= .20, random_state = 4)
+x_traincv=cv.fit_transform(X_train)
+a=x_traincv.toarray()
+x_testcv=cv.transform(X_test)
+mnb = MultinomialNB()
+y_train=y_train.astype('int')
+mnb.fit(x_traincv,y_train)
+testmessage=X_test.iloc[0]
+predictions=mnb.predict(x_testcv)
+print "Here are my predictions", predictions
+a=np.array(y_test)
+print a
+print(average_precision_score(y_test, predictions))
