b65ad627da3807897c7ce2d8c56e029360b92030
Kam <kameronkales@gmail.com>
2019-02-05 22:12:10 -0500

    Adding to heroku.:

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 
 @app.route('/', methods=['GET'])
-def test_ecs():
+def test():
     return jsonify({'response': 200})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
