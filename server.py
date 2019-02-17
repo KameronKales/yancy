ee5cc6451c8f7a27715a8ce6c23f28581e546292
Kam <kameronkales@gmail.com>
2019-02-17 16:58:46 -0500

    added sentry

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify, Response
 import json
 from routes import *
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://e46acac9ea124ad28f33b0087f803a1b@sentry.io/1396061",
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)
 app.register_blueprint(routes)
